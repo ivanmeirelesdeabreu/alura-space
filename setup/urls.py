8e5c8193b482a0fdaf448d9daa9daa88456cd7d1
ivanmeirelesdeabreu <ivanmeirelesdeabreu.gmail.com>
2023-08-25 16:53:25 -0300

    mudança

--- setup/urls.py
+++ setup/urls.py
@@ -22,6 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("galeria.urls")),
-    path("musicas/", include("musicas.urls")),
-    path("usuarios/", include("usuarios.urls")),
+    path("", include("usuarios.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
